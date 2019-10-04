821bda36ecaa26ffb094655c6e9506019bef6d10
ntipros <pntigurirwa@gmail.com>
2019-10-03 19:57:54 -0500

    modified date file

--- date.c
+++ date.c
@@ -2,27 +2,16 @@
 #include "user.h"
 #include "date.h"
 
-static char month_name[12][7] = {
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec"
-};
 int
 main(int argc, char *argv[])
 {
-  struct rtcdate r;
-  if (date(&r)) {
-    printf(2, "date failed\n");
-  }
-  printf(1, "%s %d %d:%d:%d UTC %d\n", &month_name[r.month], r.day, r.hour, r.minute, r.second, r.year);
-  exit();
+    struct rtcdate r;
+
+    if (date(&r)) {
+        printf(2, "date failed\n");
+        exit();
+    }
+
+    printf(1, "%d-%d-%d %d:%d:%d UTC\n", r.year, r.month, r.day, r.hour, r.minute, r.second);
+    exit();
 }
